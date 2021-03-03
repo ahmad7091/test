2bd607291d18db61902acc005070c6df21f08bcd
Aves <info@avestech.ir>
2021-03-03 13:52:21 +0330

    add files

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 
 const
-    io = require("socket.io"),
-    server = io.listen(3000);
+    server = require("socket.io")(3000);
+  //  server = io.listen(3000);
 
 let
     sequenceNumberByClient = new Map();
@@ -49,5 +49,5 @@ server.on("connection", (socket) => {
     	 // client.emit("deviceStatus", {user_id:'UR09370093519-382B7804F503',c_type:41,data_val1:'30559',data_val2:'EL110',data_val3:'5'});
     	  //client.emit('atime', {user_id:'HB382B7804F503',c_type:5});
 		
-	}
+//	}
 //},40000);  
